24e25f5b83ee3f3a0b4640178dc00a177d0f54d0
Zakeer Hussain <zakeer@zakeer.me>
2023-12-12 14:25:46 +0000

    debounce call and db seeding

--- ui/components/SearchBar.tsx
+++ ui/components/SearchBar.tsx
@@ -1,26 +1,111 @@
 'use client';
 
-import React, {useState} from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { Input } from "@material-tailwind/react";
 
-export default function SearchBar({searchCallback }: any) {
-    console.log("searchCallback(searchText)===", searchCallback)
-    const [searchText, setSearchText] = useState('')
-    console.log('SEARCHTEXT', searchText)
+let count = 0;
+function getCount() {
+    count++;
+    return count;
+}
 
-    const onSearch =() => {
-        searchCallback(searchText)
-        setSearchText('')
+
+
+function debounce(callback: (value: any) => void, delay = 500) {
+    let timer: any;
+    return (value: any) => {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            callback(value);
+        }, delay);
     }
+};
+
+// const fn = debounce(() => {}, 1000);
+// fn(); -> () => {}
+
+export default function SearchBar({ searchCallback }: any) {
+    const [searchText, setSearchText] = useState('text');
+    const getCourses = () => {
+        const API_URL = `/api/courses?query=${searchText}`;
+        console.log(": Search Value changed ::", searchText, API_URL);
+        fetch(API_URL)
+            .then(res => res.json())
+            .then(console.log)
+    }
+
+    const debounceApi = useCallback( debounce((text: any) => {
+        console.log(":: debounceApi ::", text)
+    }, 500), [])
+
+    useEffect(() => {
+        /*
+        console.log(":: USE EFFECT searchText", searchText)
+        let timer = setTimeout(getCourses, 500)
+        return () => {
+            console.log(":: USE EFFECT searchText in RETURN", searchText);
+            clearTimeout(timer)
+        }
+        */
+        console.log(":: useEffect ::", searchText);
+        debounceApi(searchText);
+    }, [searchText]);
+
+    /*
+        searchText = ""; old
+        searchText = "b" new
+    */
+
+
     return (
         <>
-         <div className="input-group">
-            <h1>This Search is made Using NEXTJS & Prisma...</h1>
-            <h1>NOTE: Search functionality is not yet Implemented. Working on it :) </h1>
-                <div>
-                    <input value={searchText} type="text" className="input" id="Email" name="Email" placeholder="Enter a Course Name" onChange={e => setSearchText(e.target.value)}/>
-                    <input className="button--submit" value="Search" type="submit" onClick={() => onSearch()}/>
-                </div>
-          </div>
-      </>
+            <div className="input-group">
+                <Input
+                    crossOrigin={true}
+                    value={searchText}
+                    type="text"
+                    className="input"
+                    placeholder="Enter a Course Name"
+                    onChange={e => setSearchText(e.target.value)} />
+                <p>Search Word: {searchText}</p>
+            </div>
+        </>
     )
+}
+
+
+ class Search extends React.Component {
+
+    state = {
+        searchText: ""
+    }
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    debounceApiCall = debounce(() => {
+        console.log(":Invoking: DebounceCall", this.state.searchText)
+    }, 2000)
+
+    setSearchText = (value: any) => {
+        this.setState({
+            searchText: value
+        });
+        console.log(": setSearchText :", value);
+        this.debounceApiCall(value);
+    }
+
+    render() {
+        return <div className="input-group">
+            <Input
+                crossOrigin={true}
+                value={this.state.searchText}
+                type="text"
+                className="input"
+                placeholder="Enter a Course Name"
+                onChange={e => this.setSearchText(e.target.value)} />
+            <p>Search Word: {this.state.searchText}</p>
+        </div>
+    }
 }
