b728ae6e7a9d5c054f08e7afb8e284927c47cd11
Syed Zakeer Hussain <syed.zakeer.hussain@sap.com>
2023-12-15 20:06:51 +0530

    schema changes and new api route

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,7 @@ const Prisma = require('@prisma/client');
 const { PrismaClient } = Prisma;
 
 const courses = require("./data/courses");
+const lessons = require("./data/lessons/machine-learning/data");
 
 const prisma = new PrismaClient();
 
@@ -11,12 +12,38 @@ const prisma = new PrismaClient();
 
 courses.forEach((course: any) => {
     (async () => {
-        const newCourse = await prisma.course.create({
-            data: {
+        const newCourse = await prisma.course.upsert({
+            where: {
+                id: course.id
+            },
+            update: {
+                title: course.name,
+                description: course.name
+            },
+            create: {
+                id: course.id,
                 title: course.name,
                 description: course.name
             }
         });
+    })()
+});
+
+lessons.forEach((lesson: any, idx: any) => {
+    (async () => {
+        await prisma.lesson.upsert({
+            where: {
+                id: idx+1
+            },
+            create: {
+                id: idx+1,
+                name: lesson,
+                courseId: 4
+            },
+            update: {
+
+            }
+        })
     })()
 })
 
